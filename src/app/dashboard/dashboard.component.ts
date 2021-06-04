7f46a16c0c94a132eb19563d44c3e0f325d96c3c
Shiva <e10854@excelityglobal.com>
2021-06-04 11:57:59 +0530

    tab changed

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  public activeTab: number = 2
+  public activeTab: number = 1
   constructor(private uploadService: UploadFilesService) { }
   public model: any = {};
 
